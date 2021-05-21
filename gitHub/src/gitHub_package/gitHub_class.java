bc498fe65f82829be7b85194ee3428dd14ae5a7d
AsutoshNayak <asutosh.net02@gmail.com>
2021-05-22 00:13:34 +0530

    github added

---
+++ gitHub/src/gitHub_package/gitHub_class.java
@@ -0,0 +1,11 @@
+package gitHub_package;
+
+public class gitHub_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Github added");
+	}
+
+}
