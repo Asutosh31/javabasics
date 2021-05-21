547aea9754ce4e9463bd97a8c62d987f17771f15
AsutoshNayak <asutosh.net02@gmail.com>
2021-05-22 00:19:34 +0530

    github2 added

---
+++ gitHub/src/gitHub_package/gitHub_class2.java
@@ -0,0 +1,11 @@
+package gitHub_package;
+
+public class gitHub_class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Github2 added");
+	}
+
+}
